d8a0b5a7a5904d23b186e3c30e4680eeaff3fcd0
Edgar Everton <edgareverton@outlook.com>
2025-06-16 22:43:37 -0300

    aula8

---
+++ exercicios/exer7/index.js
@@ -0,0 +1,10 @@
+// exercicio sobre const 
+
+const nome = 'Edgar';
+console.log(nome);
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+console.log(resultadoDuplicado);
