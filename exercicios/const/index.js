c9c27fb6325149b13f8639889172201feeb7bda0
Edgar Everton <edgareverton@outlook.com>
2025-07-04 16:50:15 -0300

    const

---
+++ exercicios/const/index.js
@@ -0,0 +1,11 @@
+
+const nome = `Edgar Everton`;
+const sobrenome = 'Araujo';
+const peso = 77;
+const idade = 28;
+const altura = 1.64;
+let imc; // peso / (altura * altura)
+let anoNascimento;
+
+imc = peso / (altura * altura);
+console.log(imc);
