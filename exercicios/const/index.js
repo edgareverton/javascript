75d5b29e8d55212553e2719410ced8bef48d5c5d
Edgar Everton <edgareverton@outlook.com>
2025-07-07 22:35:16 -0300

    ver 1.3

--- exercicios/const/index.js
+++ exercicios/const/index.js
@@ -7,5 +7,14 @@ const altura = 1.64;
 let imc; // peso / (altura * altura)
 let anoNascimento;
 
-imc = peso / (altura * altura);
-console.log(imc);
+imc = peso / (altura * altura);;
+anoNascimento = 2025 - idade;
+
+console.log(nome, sobrenome, 'tem', idade, 'anos,', 'pesa', peso, 'KG',)
+console.log( 'Tem ', altura, 'de altura e seu imc é: ', imc)
+console.log(nome, 'nasceu em',anoNascimento);
+
+//Opçção com $
+console.log(`\n${nome} ${sobrenome} tem ${idade} anos, pesa ${peso}KG `)
+console.log( 'Tem ', altura, 'de altura e seu imc é: ', imc)
+console.log(nome, 'nasceu em',anoNascimento);
