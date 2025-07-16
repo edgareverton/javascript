d840b6874668b8794c8cfb7cef4e80e4176faa20
Edgar Everton <edgareverton@outlook.com>
2025-07-16 19:34:38 -0300

    1.02

---
+++ operadores/index.js
@@ -0,0 +1,20 @@
+let contador = 0;
+contador += 2;
+contador += 2;
+contador += 2;
+contador += 2;
+contador += 2;
+console.log(contador);
+
+contador1 = 0;
+contador1++
+console.log(++contador1);
+
+let contadorMult = 1;
+contadorMult *= 2;
+contadorMult *= 2;
+contadorMult *= 2;
+contadorMult *= 2;
+
+console.log(contadorMult);
+
