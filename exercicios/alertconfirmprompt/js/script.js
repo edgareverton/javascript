4c1017ffb375e2d4db379b45e3bd0cd3692e8de7
Edgar Everton <edgareverton@outlook.com>
2025-07-18 00:16:18 -0300

    1

--- exercicios/alertconfirmprompt/js/script.js
+++ exercicios/alertconfirmprompt/js/script.js
@@ -1,1 +1,6 @@
-alert('Nossa Mensagem!');
+let number1 = prompt('Digite um número!');
+let number2= prompt('Digite outro número!');
+ number1 = Number(number1);
+ number2 = Number(number2);
+let resultado = number1 + number2;
+alert(`o resultado é:  ${resultado}`);
